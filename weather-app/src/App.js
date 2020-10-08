088019fbb2b740564c21c8b260bc4462c2b20996
Tabary7 <clement@wiztopic.com>
2020-10-08 17:55:04 +0200

    The weather app

---
+++ weather-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherFetch from './weatherFetch';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <WeatherFetch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/weatherFetch.js
@@ -0,0 +1,46 @@
+import React,{useState, useEffect} from "react";
+
+function WeatherFetch() {
+    const [feels_like,setFeelsLike] = useState('');
+    const [mainTemp,setMainTemp] = useState('');
+    const [mainHumidity,setMainHumidity] = useState('');
+    const [description,setDescription] = useState('');
+    const [city,setCity] = useState('');
+    const [main,setMain] = useState('');
+    const [iconID,setIconID] = useState('');
+    
+    useEffect(async () => {
+
+    	const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=8f25956cb93d426195ba1a195340cefc`);
+		const data = await response.json();
+		console.log(data);
+
+		setFeelsLike(Math.floor(data.main.feels_like - 273));
+	    setMainTemp(Math.floor(data.main.temp - 273));
+	    setMainHumidity(data.main.humidity);
+	    setCity(data.name);
+	    setDescription(data.weather[0].description);
+	    setMain(data.weather[0].main);
+	    setIconID(data.weather[0].icon);
+
+	},[])
+
+	return (
+	    <>
+		<form>
+		<br/><br/>
+			<input type="text" placeholder="Ville"/><br/><br/>
+			<button> Which ville do you want to see the Meteo ?</button>
+		</form>
+
+	    <h1>Ville : {city}</h1>
+	    <h1>Température : {mainTemp}{'\u00b0'} Celsius</h1>
+	    <h1>Ressenti: {feels_like}{'\u00b0'} Celsius</h1>
+	    <h1>Humidité: {mainHumidity} %</h1>
+	    <h1>Description du temps: {main} / {description} </h1>
+	    <h1></h1>
+	    <img src={"http://openweathermap.org/img/wn/" + iconID + "@2x.png"}/>
+	    </>
+	)
+}
+export default WeatherFetch;
